e5b4c7d722d46d4f20cf9e6a28766b39fa0e9d76
liubo <2281075105@qq.com>
2020-06-05 11:32:38 +0800

    高级配置

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 # react-manager
 
 ``` 1. yarn add react-router-dom ```
+
+``` 2. yarn add antd ```
+
+``` 3. yarn add react-app-rewired customize-cra```
+
+``` 4. yarn add babel-plugin-import ```
+
+``` 5. yarn add less@3.10.3  less-loader@5.0.0 ??:??????less-loader@5.0.0```
+
+``` 6.  ```
+
--- src/pages/admin/admin.js
+++ src/pages/admin/admin.js
@@ -1,9 +1,14 @@
 import React,{Component} from 'react'
+import {Button} from 'antd'
+
 
 export default class Admin extends Component{
     render(){
         return (
-            <div>Admin</div>
+            <div>Admin
+                <Button>测试按钮</Button>
+            </div>
+
         )
     }
 }
